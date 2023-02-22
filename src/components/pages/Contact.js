83ddb96ee78bb2732b27e540af28dfc218d0acc0
Jordan Gibbs <jgibbs291@hotmail.co.uk>
2023-02-22 18:35:37 +0000

    edited contact form

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -4,26 +4,25 @@ import Form from 'react-bootstrap/Form';
 
 export default function Contact() {
   return (
-    <Form style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center',}} >
+    <Form style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column',}} >
 
-      <Form.Group className="mb-3" controlId="formBasicEmail">
+      <Form.Group style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column', width: 1000}} className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
         <Form.Control type="email" placeholder="Enter email" />
         <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
         </Form.Text>
       </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
+      <Form.Group style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column', width: 1000 }} className="mb-3" controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
         <Form.Control type="password" placeholder="Password" />
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicCheckbox">
         </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicMessage">
+      <Form.Group style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column', width: 1000 }} className="mb-3" controlId="formBasicMessage">
         <Form.Label>Messsage</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Control type="message" placeholder="Message" />
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicCheckbox">
         

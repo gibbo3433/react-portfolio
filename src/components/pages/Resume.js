5184abbb02df5c6d989546c25f680372011041f4
Jordan Gibbs <jgibbs291@hotmail.co.uk>
2023-02-23 00:19:50 +0000

    updated and fixed resume

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,16 +1,31 @@
 import React from 'react';
-
+import Container from 'react-bootstrap/Container';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Button from 'react-bootstrap/Button';
 
 export default function Resume() {
+
+  const proficiencies = [
+    'HTML',
+    'CSS',
+    'Node.js',
+    'SQL',
+    'Handlebars',
+    'Restful APIs',
+    'React',
+  ];
+
   return (
-    <div>
-    <h1>Portfolio</h1>
-    <p>
-      ive done things, trust me bro
-    </p>
-    
-  </div>   
+    <Container>
+          <h2>My Proficiencies:</h2>
+          <ListGroup>
+            {proficiencies.map((proficiency, index) => (
+              <ListGroup.Item style={{ textAlign: 'center', fontSize: 30 }} key={index}>{proficiency}</ListGroup.Item>
+            ))}
+          </ListGroup>
+          <Button style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', marginTop: 10}} href="https://drive.google.com/file/d/1Y5PXNTqonmtDEBvysj8gGIDvn6oTQEug/view?usp=share_link" download>
+            Resume Download Button
+          </Button>
+    </Container>
   );
 }
-
-
